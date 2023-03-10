12ba60805a708e3494df2ab3aeb761a760d18552
thrallbata <vicortobdel@gmail.com>
2023-03-10 15:03:20 +0700

    complite 1 ex

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ def arithmetic_sequence_elements(a, d, n):
     return ', '.join(list_progress)
 
 
-print(arithmetic_sequence_elements(1, 2, 5))
+# print(arithmetic_sequence_elements(1, 2, 5))
 
 
 def arithmetic_sequence_elements1(a, r, n):
     return ", ".join((str(a+r*i) for i in range(n)))
 
 
-print(arithmetic_sequence_elements(1, 2, 5))
+# print(arithmetic_sequence_elements(1, 2, 5))
 
 
 def alphanumeric(password):
@@ -47,4 +47,50 @@ def password_validation(regex):
 
 regex = 'Fjd3IR9'
 #print(regex.lower())
-print(password_validation('fjd3IR9'))
+# print(password_validation('fjd3IR9'))
+
+
+def cipher_this(message):
+    # element = [elem for word in message for elem in word]
+    list_word = []
+    for i in message.split():
+        word = list(i)
+        word[0] = str(ord(word[0]))
+        elem_2 = word[1]
+        word[1] = word[len(word)-1]
+        word[len(word)-1] = elem_2
+        word.append(' ')
+        list_word += word
+    return " ".join(list_word)
+
+
+print(cipher_this("Antoi shiskin"))
+
+
+def decipher_this(message):
+    list_word = []
+    for i in message.split():
+        word = list(i)
+        count = 0
+        first_elem = ''
+        for j in word:
+            try:
+                int(j)
+                first_elem += j
+                count += 1
+            except:
+                continue
+        for o in range(0, count):
+            word.pop(0)
+        word.insert(0, chr(int(first_elem)))
+        if len(word) >= 2:
+            elem_2 = word[1]
+            word[1] = word[len(word)-1]
+            word[len(word)-1] = elem_2
+        word.append(' ')
+        list_word += word
+    del list_word[-1]
+    return "".join(list_word)
+
+
+print(decipher_this("65 119esi 111dl 111lw 108dvei 105n 97n 111ka"))
