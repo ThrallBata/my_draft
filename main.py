17a3e738f594ad10eded3982aadd076c93020cea
thrallbata <vicortobdel@gmail.com>
2023-03-20 10:59:46 +0700

    add new theory

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def cipher_this(message):
     return " ".join(list_word)
 
 
-print(cipher_this("Antoi shiskin"))
+# print(cipher_this("Antoi shiskin"))
 
 
 def decipher_this(message):
@@ -93,4 +93,24 @@ def decipher_this(message):
     return "".join(list_word)
 
 
-print(decipher_this("65 119esi 111dl 111lw 108dvei 105n 97n 111ka"))
+# print(decipher_this("65 119esi 111dl 111lw 108dvei 105n 97n 111ka"))
+
+
+import itertools
+
+
+def choose_best_sum(t, k, ls):
+    try:
+        return max(sum(i) for i in itertools.combinations(ls, k) if sum(i) <= t)
+    except:
+        return None
+
+
+xs = [100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34, 89]
+# print(choose_best_sum(230, 4, xs))
+#
+#
+# for i in itertools.combinations(xs, 3):
+#     if sum(i) <= 230:
+#         print(i)
+#         print(sum(i))
