e518d5e78761c7e0725e769d830dc4b76a51c923
thrallbata <vicortobdel@gmail.com>
2023-05-21 21:33:48 +0700

    new theory

--- main.py
+++ main.py
@@ -235,10 +235,3 @@ print(validateStackSequences([2,1,0], [1,2,0]))
 print(validateStackSequences([2,3,0,1],[0,3,2,1]))
 
 result = []
-maxcandies = max(candies)
-    for i in range(len(candies)):
-	    if candies[i]+extraCandies >= maxcandies:
-		    result.append(True)
-		else:
-			result.append(False)
-	return result
